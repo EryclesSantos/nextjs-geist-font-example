482ce73123e2746ca5f0f7a0872954c19223e78b
EryclesSantos <erycles.santos2@gmail.com>
2025-08-28 08:35:32 -0300

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1756380932052

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_a31de8ea._.js",
+  "static/chunks/src_6052a3e2._.js"
+],
+    source: "dynamic"
+});
